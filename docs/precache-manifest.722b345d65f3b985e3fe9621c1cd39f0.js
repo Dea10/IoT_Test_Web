9268e385377a819cd280f7d99554e7c8bea58af9
Daniel Espinosa <d111092.de@gmail.com>
2020-07-12 21:35:05 -0500

    build -> docs

---
+++ docs/precache-manifest.722b345d65f3b985e3fe9621c1cd39f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889628810bf05ad3bbde0e8fd1abc140",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f0573e0a671b0092b6c",
+    "url": "/static/css/main.da7ccf46.chunk.css"
+  },
+  {
+    "revision": "63e2bc25116b0beb537e",
+    "url": "/static/js/2.2c5b89a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c5b89a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f0573e0a671b0092b6c",
+    "url": "/static/js/main.19ac2cc1.chunk.js"
+  },
+  {
+    "revision": "141c3f0ae7be7b993ca8",
+    "url": "/static/js/runtime-main.ff02f795.js"
+  }
+]);
